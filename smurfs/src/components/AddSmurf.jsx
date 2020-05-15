ffe9e4a3c2ded566fcc052339250355c8921bc24
Michael Perez <mperez6473@gmail.com>
2020-05-15 12:09:12 -0500

    I have the add smurf form working now. It sends a new smurf that is defined.

--- smurfs/src/components/AddSmurf.jsx
+++ smurfs/src/components/AddSmurf.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { pushData } from '../store/actions';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const AddSmurf = (props) => {
     console.log('AddSmurf props', props);
+
+    const [smurf, setSmurf] = useState();
+
+    useEffect(() => {
+        props.pushData();
+    }, props);
+
+    const handleChanges = (e) => {
+        e.preventDefault();
+        const newerSmurf = {
+            ...smurf,
+            [e.target.name]: e.target.value,
+        };
+        return setSmurf(newerSmurf);
+    };
+    const onSubmit = (e) => {
+        props.pushData(smurf);
+        console.log('heres the smurf sent ', smurf);
+        e.preventDefault();
+    };
     return (
-        <form>
-            <label>Add Smurf Name: </label>
-            <input type="text" />
+        <form
+            onSubmit={(e) => {
+                onSubmit();
+            }}
+        >
+            <label htmlFor="name">Add Smurf Name: </label>
+            <input
+                type="text"
+                name="name"
+                placeholder="New Smurf Here"
+                onChange={handleChanges}
+                value={props.name}
+            />
+            <label htmlFor="age"> Smurf Age:</label>
+            <input
+                type="number"
+                name="age"
+                placeholder="Age"
+                onChange={handleChanges}
+            />
+            <label htmlFor="height"> Height: </label>
+            <input
+                type="number"
+                name="height"
+                placeholder="Height"
+                onChange={handleChanges}
+            />
+            <label htmlFor="id">
+                <input type="hidden" id="id" name="id" value={Date.now()} />
+            </label>
+            <button>Submit Smurf</button>
         </form>
     );
 };
